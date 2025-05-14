c0ac9fbc393f9ade244bc7cee61687e44341d9e1
hailst0rm <kevin@hailstormsec.com>
2025-05-14 13:33:30 +0200

    vscode stuff

--- users/hailst0rm/homeManagerModules/code/vscode/vscode.nix
+++ users/hailst0rm/homeManagerModules/code/vscode/vscode.nix
@@ -12,6 +12,15 @@ in {
 
       extensions = with pkgs.vscode-extensions; [
         github.copilot
+        continue.continue
+        ms-vscode.live-server
+        esbenp.prettier-vscode
+        eamodio.gitlens
+        wix.vscode-import-cost
+        # Code runner?
+        ms-vsliveshare.vsliveshare
+        pkief.material-icon-theme
+        mechatroner.rainbow-csv
       ];
 
       # Optional: Use VS Code Insiders instead of stable
@@ -22,6 +31,8 @@ in {
         "editor.inlineSuggest.enabled" = true;
         "editor.codeActionsOnSave" = {
           "source.fixAll" = true;
+        "editor.cursorSmoothCaretAnimation" = "on";
+        "editor.wordWrap" = "on";
         };
       };
     };
