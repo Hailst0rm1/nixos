bfda87cfbbd82fe1ca0fca5e7a269fb0bce813a7
hailst0rm <kevin.ponton@pm.me>
2025-06-02 12:58:04 +0200

    Nix-Laptop: testing more nixos-hardware (36 current  2025-06-02 12:57:35  24.11.20250528.78add7b  6.6.92                               *)

--- hosts/Nix-Laptop/configuration.nix
+++ hosts/Nix-Laptop/configuration.nix
@@ -14,10 +14,10 @@ in {
 
       # NixOS-Hardware
       # List: https://github.com/NixOS/nixos-hardware/blob/master/flake.nix
-      # inputs.nixos-hardware.nixosModules.common-cpu-intel
+      inputs.nixos-hardware.nixosModules.common-cpu-intel
       inputs.nixos-hardware.nixosModules.common-gpu-nvidia-disable
       # inputs.nixos-hardware.nixosModules.common-gpu-intel
-      # inputs.nixos-hardware.nixosModules.common-pc-laptop-ssd
+      inputs.nixos-hardware.nixosModules.common-pc-laptop-ssd
 
       # Secrets
       inputs.sops-nix.nixosModules.sops
