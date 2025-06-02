e6362d984de9a79c15bf92380fa8e9b4d4eaf29d
hailst0rm <kevin.ponton@pm.me>
2025-06-02 13:19:52 +0200

    Nix-Laptop:  (37 current  2025-06-02 13:19:01  24.11.20250528.78add7b  6.14.7-zen1                          *)

--- hosts/Nix-Laptop/configuration.nix
+++ hosts/Nix-Laptop/configuration.nix
@@ -14,10 +14,10 @@ in {
 
       # NixOS-Hardware
       # List: https://github.com/NixOS/nixos-hardware/blob/master/flake.nix
-      inputs.nixos-hardware.nixosModules.common-cpu-intel
-      inputs.nixos-hardware.nixosModules.common-gpu-nvidia-disable
+      # inputs.nixos-hardware.nixosModules.common-cpu-intel
+      # inputs.nixos-hardware.nixosModules.common-gpu-nvidia-disable
       # inputs.nixos-hardware.nixosModules.common-gpu-intel
-      inputs.nixos-hardware.nixosModules.common-pc-laptop-ssd
+      # inputs.nixos-hardware.nixosModules.common-pc-laptop-ssd
 
       # Secrets
       inputs.sops-nix.nixosModules.sops
@@ -86,7 +86,7 @@ in {
   hardware.bluetooth.powerOnBoot = false;
 
   system = {
-    # kernel = "zen";
+    kernel = "zen";
     bootloader = "grub";
     keyboard.colemak-se = true;
     theme = {
