6e3f970a68121e0a8d5aa0e0735efc3b2ff2d5db
hailst0rm <kevin.ponton@pm.me>
2025-06-03 12:06:57 +0200

    Nix-Laptop: other hardware modules (56 current  2025-06-03 12:06:49  25.05.20250602.10d7f8d  6.14.7-zen1                          *)

--- hosts/Nix-Laptop/configuration.nix
+++ hosts/Nix-Laptop/configuration.nix
@@ -14,11 +14,11 @@ in {
 
       # NixOS-Hardware - Seem to not work properly on this system?
       # List: https://github.com/NixOS/nixos-hardware/blob/master/flake.nix
-      # inputs.nixos-hardware.nixosModules.common-cpu-intel
+      inputs.nixos-hardware.nixosModules.common-cpu-intel
       # inputs.nixos-hardware.nixosModules.common-gpu-nvidia-disable
-      # inputs.nixos-hardware.nixosModules.common-gpu-intel
-      # inputs.nixos-hardware.nixosModules.common-pc-laptop-ssd
-      inputs.nixos-hardware.nixosModules.dell-precision-5530
+      inputs.nixos-hardware.nixosModules.common-gpu-intel
+      inputs.nixos-hardware.nixosModules.common-pc-laptop-ssd
+      # inputs.nixos-hardware.nixosModules.dell-precision-5530
 
       # Secrets
       inputs.sops-nix.nixosModules.sops
@@ -42,13 +42,6 @@ in {
   # Enables editing of hosts
   environment.etc.hosts.enable = false;
   environment.etc.hosts.mode = "0700";
-
-  boot = {
-    kernelParams = lib.mkForce [
-      "i915.enable_psr=1"
-      "nvidia_drm.modeset=0"
-    ];
-  };
 
   # ===
 
