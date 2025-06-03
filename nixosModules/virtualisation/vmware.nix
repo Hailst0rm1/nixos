1f881366ddad0889b43ba98ae896f82f39de8919
hailst0rm <kevin.ponton@pm.me>
2025-06-03 13:32:36 +0200

    Nix-Laptop: test vmware fix (63 current  2025-06-03 13:32:26  25.05.20250602.10d7f8d  6.12.31                              *)

--- nixosModules/virtualisation/vmware.nix
+++ nixosModules/virtualisation/vmware.nix
@@ -16,9 +16,9 @@ in {
     boot.kernelParams = ["transparent_hugepage=never"];
 
     # Add VMware kernel modules for zen
-    boot.extraModulePackages = lib.mkIf (config.system.kernel == "zen") [
-      pkgs.linuxKernel.packages.linux_zen.vmware
-    ];
+    # boot.extraModulePackages = lib.mkIf (config.system.kernel == "zen") [
+    #   pkgs.linuxKernel.packages.linux_zen.vmware
+    # ];
 
     # Dark theme (applied in HM-stylix manually)
     environment.systemPackages = [pkgs.gnome-themes-extra];
