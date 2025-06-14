4e858a0d7987ccd565de143e08726ba043e54e57
hailst0rm <kevin.ponton@pm.me>
2025-06-14 16:03:40 +0200

    Nix-Workstation: fixed swww and refind (14 current  2025-06-14 16:03:29  25.05.20250612.fd48718  6.12.33                          *)

--- users/hailst0rm/homeManagerModules/hyprland/swww.nix
+++ users/hailst0rm/homeManagerModules/hyprland/swww.nix
@@ -12,13 +12,13 @@
       BG=`find ${../wallpapers} -name "*.gif" | shuf -n1`
       if pgrep swww-daemon >/dev/null; then
         swww img "$BG" \
-          --resize fit \
           --transition-fps 60 \
           --transition-duration 2 \
           --transition-type random \
           --transition-pos top-right \
           --transition-bezier .3,0,0,.99 \
           --transition-angle 135 || true
+          # --resize fit bugged on 0.10.2
         sleep 1800
       else
         (swww-daemon 1>/dev/null 2>/dev/null &) || true
