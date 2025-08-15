24b448cb3c04b60ee49bec48854014f933b18ca8
hailst0rm <kevin.ponton@pm.me>
2025-08-15 12:21:46 +0200

    Nix-Laptop: ligolo-mp, username-anarchy and flake update (120 current  2025-08-15 12:21:21  25.05.20250813.3385ca0  6.16.0-zen1                          *)

--- users/hailst0rm/homeManagerModules/cyber/red-tools.nix
+++ users/hailst0rm/homeManagerModules/cyber/red-tools.nix
@@ -10,7 +10,41 @@
   nixosDir = inputs.self;
   thc-hydra = pkgs-unstable.callPackage "${nixosDir}/pkgs/thc-hydra/package.nix" {};
   autorecon = pkgs-unstable.callPackage "${nixosDir}/pkgs/autorecon/package.nix" {};
+  # ipcrawler = pkgs-unstable.callPackage "${nixosDir}/pkgs/ipcrawler/package.nix" {};
   ipmap = builtins.readFile ./files/ipmap.sh;
+
+  ligolo-mp = pkgs.stdenv.mkDerivation {
+    pname = "ligolo-mp";
+    version = "2.1.0";
+
+    src = pkgs.fetchurl {
+      url = "https://github.com/ttpreport/ligolo-mp/releases/download/v2.1.0/ligolo-mp_linux_amd64";
+      sha256 = "sha256-W4k2ExJk5P4pjNvvmBilrL++3Haz9LMxbWKRWEsnYaI=";
+    };
+
+    phases = ["installPhase"];
+    installPhase = ''
+      mkdir -p $out/bin
+      install -m755 $src $out/bin/ligolo-mp
+    '';
+  };
+
+  # Postex-Tools
+  rubeus = pkgs.stdenv.mkDerivation {
+    pname = "rubeus";
+    version = "4.8.1-compiled";
+
+    src = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/r3motecontrol/Ghostpack-CompiledBinaries/master/dotnet%20v4.8.1%20compiled%20binaries/Rubeus.exe";
+      sha256 = "sha256-QKS15U/szlLJ2O9bL6OXOj3XSMW87de94RVKpKk2wuE=";
+    };
+
+    phases = ["installPhase"];
+    installPhase = ''
+      mkdir -p $out
+      install -m755 $src $out/Rubeus.exe
+    '';
+  };
 in {
   # options.redTools.enable = lib.mkEnableOption "Enable Red Tooling";
 
@@ -21,20 +55,22 @@ in {
         "cyber/hashcat-rules".source = "${pkgs-unstable.hashcat}/share/doc/hashcat/rules";
         "cyber/john-rules/john.conf".source = "${pkgs-unstable.john}/etc/john/john.conf";
         "cyber/metasploit/win-revtcp-listener.rc".source = ./files/win-revtcp-listener.rc;
-        ".config/AutoRecon/config.toml".source = ./files/autorecon-config.toml;
+        "cyber/AutoRecon/config.toml".source = ./files/autorecon-config.toml;
         # "cyber/postex-tools/SharpHound.ps1".source = "${pkgs-unstable.bloodhound}/resources/app/Collectors/SharpHound.ps1";
         # "cyber/ligolo/config.yaml".source = ./files/ligolo-config.yaml;
       };
 
       # Allows me to bypass read-only fs
       activation.copyTools = lib.hm.dag.entryAfter ["writeBoundary"] ''
-        mkdir -p "${config.home.homeDirectory}/cyber/ligolo"
         mkdir -p "${config.home.homeDirectory}/cyber/postex-tools"
 
-        cp -f ${builtins.toPath ./files/ligolo-config.yaml} "${config.home.homeDirectory}/ligolo.yaml"
-        cp -f ${pkgs-unstable.bloodhound}/lib/BloodHound/resources/app/Collectors/SharpHound.ps1 "${config.home.homeDirectory}/cyber/postex-tools/SharpHound.ps1"
+        # mkdir -p "${config.home.homeDirectory}/cyber/ligolo"
+        # cp -f ${builtins.toPath ./files/ligolo-config.yaml} "${config.home.homeDirectory}/ligolo.yaml"
+        # chmod 666 "${config.home.homeDirectory}/ligolo.yaml"
 
-        chmod 666 "${config.home.homeDirectory}/ligolo.yaml"
+        cp -f ${pkgs-unstable.bloodhound}/lib/BloodHound/resources/app/Collectors/SharpHound.ps1 "${config.home.homeDirectory}/cyber/postex-tools/SharpHound.ps1"
+        cp -f ${rubeus}/Rubeus.exe "${config.home.homeDirectory}/cyber/postex-tools/Rubeus.exe"
+
       '';
 
       sessionVariables = {
@@ -43,6 +79,7 @@ in {
         # === Testing corner ===
         # wireshark
         wineWowPackages.wayland
+        ligolo-mp
 
         # === Reconnaissance ===
 
@@ -73,6 +110,7 @@ in {
         nuclei # Vulnerability scanner
 
         # Web
+        # ipcrawler # Identify best wordlist
         whatweb # Web scanner (meta)
         nikto # Another web scanner
         gobuster # Directory busting
@@ -127,6 +165,7 @@ in {
         # === Wordlists ===
         cewl # Wordlist generator based on website
         crunch # Easy wordlist generator
+        username-anarchy # Username generator
         wordlists # Note: This includes seclists
         # cd $(wordlists_path) # Go to wordlists
         # <command> $(wordlists_path)/rockyou.txt # Use wordlist
@@ -137,6 +176,7 @@ in {
           ${config.browser} "${cyberchef}/share/cyberchef/index.html"
         '')
         (writeShellScriptBin "ipmap" ipmap) # Map ip to hostname
+        go # Required by ligolo-mp
       ];
     };
   };
