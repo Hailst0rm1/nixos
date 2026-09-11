fb1dac0d9c5c8a9d392459d553334182fb38b6db
Kevin Ponton <kevin.ponton@pm.me>
2026-09-11 07:03:15 +0200

    Automated Nix package and agent input updates (2026-09-11)

--- pkgs/codeburn/package.nix
+++ pkgs/codeburn/package.nix
@@ -10,7 +10,7 @@
   # upstream's main branch never reach this derivation until the hash
   # changes. Bump `litellmRelease` to a newer https://github.com/BerriAI/litellm
   # release tag and refresh the hash to pick up new model prices.
-  litellmRelease = "v1.100.0";
+  litellmRelease = "v1.100.1";
   litellmPricing = fetchurl {
     url = "https://raw.githubusercontent.com/BerriAI/litellm/refs/tags/${litellmRelease}/model_prices_and_context_window.json";
     hash = "sha256-qYD6DiB9CXaj+njGp8MWXsPBz9grDc//wqVXlbn29FE=";
